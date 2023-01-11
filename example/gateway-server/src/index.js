401be195379442e9e1ce82f5126c7cee697f8c4b
Alexandra Capatina <46926675+alexandra-c@users.noreply.github.com>
2023-01-11 13:48:51 +0200

    reference repository

--- example/gateway-server/src/index.js
+++ example/gateway-server/src/index.js
@@ -8,24 +8,24 @@ import {
 const isProd = process.env.NODE_ENV === "production";
 const apolloKey = process.env.APOLLO_KEY;
 
-let gatewayOptions = {
-  debug: isProd ? false : true
-};
+// let gatewayOptions = {
+//   debug: isProd ? false : true
+// };
 
-if (!apolloKey) {
-  console.log("Head to https://studio.apollographql.com an create an account");
+// if (!apolloKey) {
+//   console.log("Head to https://studio.apollographql.com an create an account");
 
-  gatewayOptions.serviceList = [
-    { name: "authors", url: process.env.AUTHORS_SERVICE_URL },
-    { name: "posts", url: process.env.POSTS_SERVICE_URL }
-  ];
-}
+//   gatewayOptions.serviceList = [
+//     { name: "authors", url: process.env.AUTHORS_SERVICE_URL },
+//     { name: "posts", url: process.env.POSTS_SERVICE_URL }
+//   ];
+// }
 
 const apolloUsageReportingPlugin = apolloKey
   ? ApolloServerPluginUsageReporting()
   : ApolloServerPluginUsageReportingDisabled();
 
-const gateway = new ApolloGateway(gatewayOptions);
+const gateway = new ApolloGateway();
 const server = new ApolloServer({
   gateway,
   subscriptions: false,
