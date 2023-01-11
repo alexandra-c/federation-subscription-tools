92302be6943a48b0aa633a9388e2519b7ce78367
Alexandra Capatina <46926675+alexandra-c@users.noreply.github.com>
2023-01-11 13:55:19 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { addGatewayDataSourceToSubscriptionContext } from "./utils/subscriptions";
+export { mergeGatewayDataSources } from "./utils/subscriptions";
 export { GatewayDataSource } from "./datasources/GatewayDataSource";
 export { getGatewayApolloConfig, makeSubscriptionSchema } from "./utils/schema";
